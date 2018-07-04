61bdab4df527f8287d158678042ec6500d1348f4
tamir2017 <897885849@qq.com>
2018-07-04 16:35:56 +0800

    add item

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -1,10 +1,12 @@
 package com.taotao.service;
 
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.pojo.TbItem;
 
 public interface ItemService {
 	
 	TbItem getItemById(Long itemId);
 	EasyUIDataGridResult getItemList(int page, int rows);
+	TaotaoResult createItem(TbItem item, String desc);
 }
--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -1,5 +1,6 @@
 package com.taotao.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.IDUtils;
+import com.taotao.mapper.TbItemDescMapper;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemDesc;
 import com.taotao.pojo.TbItemExample;
 import com.taotao.pojo.TbItemExample.Criteria;
 import com.taotao.service.ItemService;
@@ -19,6 +24,8 @@ public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private TbItemMapper itemMapper;
+	@Autowired
+	private TbItemDescMapper itemDescMapper;
 	@Override
 	public TbItem getItemById(Long itemId) {
 		//TbItem item = itemMapper.selectByPrimaryKey(itemId);
@@ -50,5 +57,31 @@ public class ItemServiceImpl implements ItemService {
 		result.setRows(list);
 		
 		return result;
+	}
+
+	@Override
+	public TaotaoResult createItem(TbItem item, String desc) {
+		//生成商品ID
+		long itemId = IDUtils.genItemId();
+		//补全TbItem属性
+		item.setId(itemId);
+		//商品状态，1-正常，2-下架，3-删除
+		item.setStatus((byte) 1);
+		//创建时间和更新时间
+		Date date = new Date();
+		item.setCreated(date);
+		item.setUpdated(date);
+		//插入商品表
+		itemMapper.insert(item);
+		//商品描述
+		TbItemDesc itemDesc = new TbItemDesc();
+		itemDesc.setItemId(itemId);
+		itemDesc.setItemDesc(desc);
+		itemDesc.setCreated(date);
+		itemDesc.setUpdated(date);
+		//插入商品表述
+		itemDescMapper.insert(itemDesc);
+		
+		return TaotaoResult.ok();
 	}
 }
