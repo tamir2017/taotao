63a84abedea7392f741137c02f19ded4142ee5c8
tamir2017 <897885849@qq.com>
2018-07-08 13:26:13 +0800

    index ad

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -1,14 +1,37 @@
 package com.taotao.portal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.portal.service.ContentService;
 
 @Controller
 public class IndexController {
+	
+	@Autowired
+	private ContentService ContentService;
 
 	@RequestMapping("/index")
-	public String showIndex() {
+	public String showIndex(Model model) {
+		//取大广告位内容
+		String json = ContentService.getAd1List();
+		//传递给页面
+		model.addAttribute("ad1", json);
+		
 		return "index";
 	}
+	
+	@RequestMapping(value="/posttest",method=RequestMethod.POST)
+	@ResponseBody
+	public String postTest(String name, String pass) {
+		System.out.println(name);
+		System.out.println(pass);
+		return "OK";
+	}
+	
 }
 
